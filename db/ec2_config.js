85eebccc4166822d364114721294ef9a044968b8
Eco-Cult <70007776+Eco-Cult@users.noreply.github.com>
2020-08-29 00:55:41 +1000

    Create ec2_config.js

---
+++ db/ec2_config.js
@@ -0,0 +1,14 @@
+//creating db connection 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    port:'3306',
+    database:'carbcon',
+    multipleStatements: true
+});
+
+//exported- to be used outside this file
+module.exports = connection;
