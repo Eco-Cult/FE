f72eecb607b42179abd56dc1776831ae9abda62b
Eco-Cult <70007776+Eco-Cult@users.noreply.github.com>
2020-08-29 01:00:27 +1000

    Create local_config.js

---
+++ db/local_config.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'nisha',
+    password: 'login123',
+    port:'3306',
+    database:'verdant',
+    multipleStatements: true
+});
+
+//exported- to be used outside this file
+module.exports = connection;
